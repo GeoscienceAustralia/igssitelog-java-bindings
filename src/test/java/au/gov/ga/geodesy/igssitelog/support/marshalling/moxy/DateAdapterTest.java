605b501081d67d9d5144c8d64b84e2bd08416a44
Brooke Smith <brooke.smith@ga.gov.au>
2016-06-09 17:09:15 +1000

    Fix missing change after recent Date to Instant mod

---
+++ src/test/java/au/gov/ga/geodesy/igssitelog/support/marshalling/moxy/DateAdapterTest.java
@@ -0,0 +1,27 @@
+package au.gov.ga.geodesy.igssitelog.support.marshalling.moxy;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.matchers.JUnitMatchers;
+
+import java.time.Instant;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by brookes on 9/06/16.
+ */
+public class DateAdapterTest {
+    DateAdapter dateAdapter = new DateAdapter();
+    @Test
+    public void testUnmarshalDateWIthWhiteSpace() throws Exception {
+        String testDate = "1939-08-12\n    ";
+        String expected = "1939-08-12";
+        Instant actual = dateAdapter.unmarshal(testDate);
+        MatcherAssert.assertThat(actual.toString(), Matchers.containsString(expected));
+    }
+
+}
