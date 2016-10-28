485533270cef74f4099002163e6b6e34be134756
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-10-28 14:38:07 +1100

    TRAVIS_PULL_REQUEST is never 'true'
    It can be 'false' or a number.

--- travis/build.sh
+++ travis/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if [ ${TRAVIS_PULL_REQUEST} = 'true' ]; then
-    nix-shell --command "mvn --settings travis/maven-settings.xml test"
-else
+if [ ${TRAVIS_PULL_REQUEST} = 'false' ]; then
     nix-shell --command "mvn --settings travis/maven-settings.xml deploy"
+else
+    nix-shell --command "mvn --settings travis/maven-settings.xml test"
 fi
