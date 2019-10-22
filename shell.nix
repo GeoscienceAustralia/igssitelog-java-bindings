3e943192ca6987b25b6357ea8239debe1f96dc88
Frank Fu <frank.fu@ga.gov.au>
2019-10-22 16:34:31 +1100

    Upgrade nixpkgs-channels to latest version @2019-10-15

--- shell.nix
+++ shell.nix
@@ -3,8 +3,8 @@ let
   pinnedPkgs = import (defaultPkgs.fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs-channels";
-    rev = "f0fac3b578086066b47360de17618448d066b30e"; # 17 April 2017
-    sha256 = "1mpwdminwk1wzycwmgi2c2kwpbcfjwmxiakn7bmvvsaxb30gwyyb";
+    rev = "94500c93dc239761bd144128a1684abcd08df6b7"; # 15 Oct 2019
+    sha256 = "0p6bd16mb1k5c3zihl1c3d62m419rii102xfb340pccgbd8j34bc";
   }) {};
 
 in
