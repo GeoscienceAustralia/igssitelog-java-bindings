8c09b9c658357cee5304e1598637cd55d1df0614
Frank Fu <frank.fu@ga.gov.au>
2019-10-24 12:19:48 +1100

    Add encripted TRAVIS_TOKEN back to .travis.yml and remove unused maven-settings.xml

--- travis/build.sh
+++ travis/build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-export AWS_DEFAULT_REGION=ap-southeast-2
-
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    nix-shell --command "mvn --settings travis/maven-settings.xml deploy"
+    nix-shell --command "mvn deploy"
 else
-    nix-shell --command "mvn --settings travis/maven-settings.xml test"
+    nix-shell --command "mvn test"
 fi
